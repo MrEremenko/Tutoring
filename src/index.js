eb7ebc03a817c021c4374d5967506c80881016d1
nerdromere <illarion@gradcent.com>
2021-11-01 06:05:09 -0700

    switching to react-ga4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-DLVQMH2GLR');
+ReactGA.send("pageview")
 
 ReactDOM.render(
     <App />,
